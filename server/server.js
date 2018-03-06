06ac342b0ea756d5744fa76d583f05f11b007978
Evgenij Koronin <ekoronin@users.noreply.github.com>
2018-03-06 12:59:36 +0100

    send message to the rooms

--- server/server.js
+++ server/server.js
@@ -70,8 +70,10 @@ io.on("connection", (socket)=>{
 
 
   socket.on("createMessage", (message, callback)=>{
-    console.log("Create message", message);
-    io.emit("newMessage", generateMessage( message.from,  message.text));// to everybody connected
+    var user = users.getUser(socket.id);
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit("newMessage", generateMessage( user.name,  message.text));
+    }
     callback();
     // socket.broadcast.emit("newMessage", {
     //   from: message.from,
@@ -83,7 +85,10 @@ io.on("connection", (socket)=>{
   });//createMessage
 
   socket.on("createLocationMessage", (coords, callback)=>{
-    io.emit("newLocationMessage", generateLocationMessage("Admin", coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("newLocationMessage", generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
     callback();
   });
 
